7237f0aa84decafa27f26a3dbb325ad83b98cfa1
colabear754 <jky2406@gmail.com>
2026-01-30 13:43:21 +0900

    🐛[Fix] 스케줄러 활성화 누락 수정

--- src/main/kotlin/com/colabear754/kbo_scraper/KboScraperApplication.kt
+++ src/main/kotlin/com/colabear754/kbo_scraper/KboScraperApplication.kt
@@ -5,9 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableJpaAuditing
 @EnableCaching
+@EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class KboScraperApplication
